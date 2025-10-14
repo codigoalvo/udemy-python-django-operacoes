bccea51da0d46ca2e99a7dcf2cc12febb3558d31
Cassio Reinaldo Amaral <codigoalvo@gmail.com>
2025-10-14 13:18:59 -0300

    Adicionando modulos de Pessoa e Diligencia

--- enderecos/models.py
+++ enderecos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Endereco(models.Model):
     logradouro = models.CharField(max_length=255, blank=False, null=False)
     bairro = models.CharField(max_length=255, blank=False, null=False)
